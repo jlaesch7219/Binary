b55f52fd2a2c8fbe2d0ebaede099029aff620608
Jackson Laesch <jlaesch7219@stu.d214.org>
2024-09-09 09:42:47 -0500

    Initial Commit

---
+++ Binary/BinaryApp.swift
@@ -0,0 +1,32 @@
+//
+//  BinaryApp.swift
+//  Binary
+//
+//  Created by Jackson A. Laesch on 9/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BinaryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Binary/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Binary
+//
+//  Created by Jackson A. Laesch on 9/9/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
