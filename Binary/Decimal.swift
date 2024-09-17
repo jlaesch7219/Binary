aa43651525b8761a53cf96d364a1f347c0a81078
Jackson Laesch <jlaesch7219@stu.d214.org>
2024-09-17 09:04:18 -0500

    csacc

---
+++ Binary/Decimal.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Binary
+//
+//  Created by Jackson A. Laesch on 9/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DecimalView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
